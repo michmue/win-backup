0e60fb863d56f19141a15a593e9aad0d6d5a34e7
michael mueller <dev@michmue.de>
2022-10-29 11:16:04 +0200

    fixes bin/Add_Features.ps1. error & duplicated Hypber-V installtion fixed

--- bin/Add_Features.ps1
+++ bin/Add_Features.ps1
@@ -3,17 +3,19 @@ if (!(Get-InstalledModule PolicyFileEditor)) {
     Install-Module -Name PolicyFileEditor  -Scope CurrentUser
 }
 
-Get-WindowsCapability -online | where $_.Name -Match "MediaFeaturePack" | Add-WindowsCapability -Online
-Get-WindowsCapability -online | where $_.Name -Match "OpenSSH.Client" | Add-WindowsCapability -Online
+Get-WindowsCapability -online | where Name -Match "MediaFeaturePack" | Add-WindowsCapability -Online
+Get-WindowsCapability -online | where Name -Match "OpenSSH.Client" | Add-WindowsCapability -Online
 
 
 #Get-WindowsOptionalFeature -Online | where Name -Match "Microsoft-Windows-Subsystem-Linux"      
 Get-WindowsOptionalFeature -Online | where FeatureName -Match "NetFx3" | Enable-WindowsOptionalFeature -Online
 Get-WindowsOptionalFeature -Online | where FeatureName -Match "NetFx4-AdvSrvs" | Enable-WindowsOptionalFeature -Online
-Get-WindowsOptionalFeature -Online | where FeatureName -Match "Microsoft-Hyper-V-All" | Enable-WindowsOptionalFeature -Online
-Get-WindowsOptionalFeature -Online | where FeatureName -Match "Microsoft-Hyper-V" | Enable-WindowsOptionalFeature -Online
-Get-WindowsOptionalFeature -Online | where FeatureName -Match "Microsoft-Hyper-V-Tools-All" | Enable-WindowsOptionalFeature -Online
-Get-WindowsOptionalFeature -Online | where FeatureName -Match "Microsoft-Hyper-V-Management-PowerShell" | Enable-WindowsOptionalFeature -Online
-Get-WindowsOptionalFeature -Online | where FeatureName -Match "Microsoft-Hyper-V-Hypervisor" | Enable-WindowsOptionalFeature -Online
-Get-WindowsOptionalFeature -Online | where FeatureName -Match "Microsoft-Hyper-V-Services" | Enable-WindowsOptionalFeature -Online
-Get-WindowsOptionalFeature -Online | where FeatureName -Match "Microsoft-Hyper-V-Management-Clients" | Enable-WindowsOptionalFeature -Online
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+#Get-WindowsOptionalFeature -Online | where FeatureName -Match "Microsoft-Hyper-V-All" | Enable-WindowsOptionalFeature -Online
+#Get-WindowsOptionalFeature -Online | where FeatureName -Match "Microsoft-Hyper-V" | Enable-WindowsOptionalFeature -Online
+#Get-WindowsOptionalFeature -Online | where FeatureName -Match "Microsoft-Hyper-V-Tools-All" | Enable-WindowsOptionalFeature -Online
+#Get-WindowsOptionalFeature -Online | where FeatureName -Match "Microsoft-Hyper-V-Management-PowerShell" | Enable-WindowsOptionalFeature -Online
+#Get-WindowsOptionalFeature -Online | where FeatureName -Match "Microsoft-Hyper-V-Hypervisor" | Enable-WindowsOptionalFeature -Online
+#Get-WindowsOptionalFeature -Online | where FeatureName -Match "Microsoft-Hyper-V-Services" | Enable-WindowsOptionalFeature -Online
+#Get-WindowsOptionalFeature -Online | where FeatureName -Match "Microsoft-Hyper-V-Management-Clients" | Enable-WindowsOptionalFeature -Online
