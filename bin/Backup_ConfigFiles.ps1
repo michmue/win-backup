a810271f046c74f76c6a7b2f73b6d460d2a9e6a2
michael mueller <dev@michmue.de>
2022-11-02 16:07:46 +0100

    fix policies copy, switch policies files, added script arguments

--- bin/Backup_ConfigFiles.ps1
+++ bin/Backup_ConfigFiles.ps1
@@ -1,3 +1,39 @@
+param(
+	[Switch]$UseDisabledDriverPOL,
+	[Switch]$UseNotepadPlusPlus,
+	[Switch]$UseJDownloader,
+	[Switch]$UseFirefox,
+	[Switch]$UsePowershell,
+	[Switch]$UseZIP7,
+	[Switch]$UseGit,
+	[Switch]$UsePolicy,
+	[Switch]$UseSyncthing,
+	[Switch]$DisableAll,
+	[Switch]$EnableAll
+)
+
+if ($DisableAll) {
+	$UseNotepadPlusPlus = $false
+	$UseJDownloader = $false
+	$UseFirefox = $false
+	$UsePowershell = $false
+	$UseZIP7 = $false
+	$UseGit = $false
+	$UseSyncthing = $false
+	$UsePolicy = $false
+}
+
+if ($EnableAll) {
+	$UseNotepadPlusPlus = $true
+	$UseJDownloader = $true
+	$UseFirefox = $true
+	$UsePowershell = $true
+	$UseZIP7 = $true
+	$UseGit = $true
+	$UseSyncthing = $true
+	$UsePolicy = $true
+}
+
 $CONFIG_DESTINATION_FOLDER = Read-Host -Prompt "Path where to save the configs"
 $RESULT = @()
 
@@ -26,85 +62,103 @@ if ( !(Test-Path $CONFIG_DESTINATION_FOLDER -PathType Container) ) {
 
 
 # POLICY
-copy $MACHINE_POLICIE_FILE $CONFIG_DESTINATION_FOLDER\MachineRegistry.pol
-copy $USER_POLICIE_FILE $CONFIG_DESTINATION_FOLDER\UserRegistry.pol
-$RESULT += "[X] Policies expoted"
+if ($UsePolicy) {
+	copy $MACHINE_POLICIE_FILE $CONFIG_DESTINATION_FOLDER\Machine_DisableDriver.pol
+	copy $MACHINE_POLICIE_FILE $CONFIG_DESTINATION_FOLDER\Machine.pol
+	copy $USER_POLICIE_FILE $CONFIG_DESTINATION_FOLDER\User.pol	
+	$RESULT += "[X] Policies expoted"
+}
 
 
 # POWERSHELL
-if ( Test-Path $POWERSHELL_USER_PROFILE_FOLDER){ 
-    copy $POWERSHELL_USER_PROFILE_FOLDER\ $CONFIG_DESTINATION_FOLDER\ -Recurse -Force -ErrorAction SilentlyContinue 
-    $RESULT += "[X] Powershell User Profile expoted"
-} else { $RESULT += "[ ] Powershell User Profile not found" }
+if ($UsePowershell) {
+	if ( Test-Path $POWERSHELL_USER_PROFILE_FOLDER){ 
+		copy $POWERSHELL_USER_PROFILE_FOLDER\ $CONFIG_DESTINATION_FOLDER\ -Recurse -Force -ErrorAction SilentlyContinue 
+		$RESULT += "[X] Powershell User Profile expoted"
+	} else { $RESULT += "[ ] Powershell User Profile not found" }
+}
 
 
 # GIT
-foreach ($path in $GIT_CONFIG_FILES) {
-    mkdir $CONFIG_DESTINATION_FOLDER\git\ -ErrorAction SilentlyContinue > $null
-    if ( Test-Path $path ) {
-        copy $path $CONFIG_DESTINATION_FOLDER\git\
-    }
+if ($UseGit) {
+	foreach ($path in $GIT_CONFIG_FILES) {
+		mkdir $CONFIG_DESTINATION_FOLDER\git\ -ErrorAction SilentlyContinue > $null
+		if ( Test-Path $path ) {
+			copy $path $CONFIG_DESTINATION_FOLDER\git\
+		}
+	}
+	$RESULT += "[X] GIT Configs exported"
 }
-$RESULT += "[X] GIT Configs exported"
+
 
 # NOTEPAD++
-if ( Test-Path $NOTEPAD_PP_CONFIG_FODLER){ 
-    copy $NOTEPAD_PP_CONFIG_FODLER\ $CONFIG_DESTINATION_FOLDER -Recurse -Force -ErrorAction SilentlyContinue 
-    
-    $RESULT += "[X] NOTEPAD++ Configs exported"
-} else { $RESULT += "[ ] NOTEPAD++ not installed" }
+if ($UseNotepadPlusPlus) {
+	if ( Test-Path $NOTEPAD_PP_CONFIG_FODLER){ 
+		copy $NOTEPAD_PP_CONFIG_FODLER\ $CONFIG_DESTINATION_FOLDER -Recurse -Force -ErrorAction SilentlyContinue 
+		
+		$RESULT += "[X] NOTEPAD++ Configs exported"
+	} else { $RESULT += "[ ] NOTEPAD++ not installed" }
+}
 
 
 # 7-ZIP
-if ( Test-Path $ZIP7_CONFIG_REGISTRY.Replace("HKCU\","HKCU:") ) { 
-    reg export $ZIP7_CONFIG_REGISTRY "$CONFIG_DESTINATION_FOLDER\7zip.reg" /y > $null
-    
-    $RESULT += "[X] 7-ZIP registry file exported"
-} else { $RESULT += "[ ] 7-ZIP++ not installed" }
+if ($UseZIP7) {
+	if ( Test-Path $ZIP7_CONFIG_REGISTRY.Replace("HKCU\","HKCU:") ) { 
+		reg export $ZIP7_CONFIG_REGISTRY "$CONFIG_DESTINATION_FOLDER\7zip.reg" /y > $null
+		
+		$RESULT += "[X] 7-ZIP registry file exported"
+	} else { $RESULT += "[ ] 7-ZIP++ not installed" }
+}
 
 
 # JDOWNLOADER
-if ( Test-Path $JDOWNLOADER_CONFIG_FOLDER){
-    copy $JDOWNLOADER_CONFIG_FOLDER\ $CONFIG_DESTINATION_FOLDER -Recurse -Force -ErrorAction SilentlyContinue 
-    
-    $RESULT += "[X] JDownloader cfg Folder exported"
-} else { $RESULT += "[ ] JDownloader not installed" }
+if ($UseJDownloader) {
+	if ( Test-Path $JDOWNLOADER_CONFIG_FOLDER){
+		copy $JDOWNLOADER_CONFIG_FOLDER\ $CONFIG_DESTINATION_FOLDER -Recurse -Force -ErrorAction SilentlyContinue 
+		
+		$RESULT += "[X] JDownloader cfg Folder exported"
+	} else { $RESULT += "[ ] JDownloader not installed" }
+}
 
 
 # FIREFOX
-$firefox_profile_folders = @(Get-ChildItem -Path $FIREFOX_CONFIG_FOLDER)
-if ( $firefox_profile_folders.Length -gt 0 ) {
-    foreach ($profile in $firefox_profile_folders) {
-        if ($profile -match "default-release") {
-            Write-Host "copying $profile to $CONFIG_DESTINATION_FOLDER ..."
-            copy $FIREFOX_CONFIG_FOLDER\$profile\ $CONFIG_DESTINATION_FOLDER -Recurse -Force -ErrorAction SilentlyContinue 
-        
-            $RESULT += "[X] Firefox profile exported"
-        }
-    }
-} else { $RESULT += "[ ] Firefox not installed" }
+if ($UseFirefox) {
+	$firefox_profile_folders = @(Get-ChildItem -Path $FIREFOX_CONFIG_FOLDER)
+	if ( $firefox_profile_folders.Length -gt 0 ) {
+		foreach ($profile in $firefox_profile_folders) {
+			if ($profile -match "default-release") {
+				Write-Host "copying $profile to $CONFIG_DESTINATION_FOLDER ..."
+				copy $FIREFOX_CONFIG_FOLDER\$profile\ $CONFIG_DESTINATION_FOLDER -Recurse -Force -ErrorAction SilentlyContinue 
+			
+				$RESULT += "[X] Firefox profile exported"
+			}
+		}
+	} else { $RESULT += "[ ] Firefox not installed" }
+}
 
 
 # SYNCTHING
-$SYNCTRAYZOR_CONFIG_FILE = "$env:APPDATA\SyncTrayzor\config.xml"
-$SYNCTHING_CONFIG_FILE_1 = "$env:LOCALAPPDATA\Syncthing\config.xml"
-#$SYNCTHING_CONFIG_FILE_2 = "$env:LOCALAPPDATA\Syncthing\config.xml.v36"
-$SYNCTHING_CONFIG_FILE_3 = "$env:LOCALAPPDATA\Syncthing\key.pem"
-$SYNCTHING_CONFIG_FILE_4 = "$env:LOCALAPPDATA\Syncthing\cert.pem"
+if ($UseSyncthing) {
+	$SYNCTRAYZOR_CONFIG_FILE = "$env:APPDATA\SyncTrayzor\config.xml"
+	$SYNCTHING_CONFIG_FILE_1 = "$env:LOCALAPPDATA\Syncthing\config.xml"
+	#$SYNCTHING_CONFIG_FILE_2 = "$env:LOCALAPPDATA\Syncthing\config.xml.v36"
+	$SYNCTHING_CONFIG_FILE_3 = "$env:LOCALAPPDATA\Syncthing\key.pem"
+	$SYNCTHING_CONFIG_FILE_4 = "$env:LOCALAPPDATA\Syncthing\cert.pem"
 
 if ( (Test-Path $SYNCTRAYZOR_CONFIG_FILE) -and
      (Test-Path $SYNCTHING_CONFIG_FILE_1) -and
      (Test-Path $SYNCTHING_CONFIG_FILE_3) -and
 	 (Test-Path $SYNCTHING_CONFIG_FILE_4)){
 
-    copy $SYNCTRAYZOR_CONFIG_FILE $CONFIG_DESTINATION_FOLDER\synctrayzor_config.xml
-    copy $SYNCTHING_CONFIG_FILE_1 $CONFIG_DESTINATION_FOLDER
-#    copy $SYNCTHING_CONFIG_FILE_2 $CONFIG_DESTINATION_FOLDER
-    copy $SYNCTHING_CONFIG_FILE_3 $CONFIG_DESTINATION_FOLDER
-    copy $SYNCTHING_CONFIG_FILE_4 $CONFIG_DESTINATION_FOLDER
-    
-    $RESULT += "[X] Syncthing exported"
-} else { $RESULT += "[ ] Syncthing not installed" }
+		copy $SYNCTRAYZOR_CONFIG_FILE $CONFIG_DESTINATION_FOLDER\synctrayzor_config.xml
+		copy $SYNCTHING_CONFIG_FILE_1 $CONFIG_DESTINATION_FOLDER
+	#    copy $SYNCTHING_CONFIG_FILE_2 $CONFIG_DESTINATION_FOLDER
+		copy $SYNCTHING_CONFIG_FILE_3 $CONFIG_DESTINATION_FOLDER
+		copy $SYNCTHING_CONFIG_FILE_4 $CONFIG_DESTINATION_FOLDER
+		
+		$RESULT += "[X] Syncthing exported"
+	} else { $RESULT += "[ ] Syncthing not installed" }
+}
 
 
 # END
--- bin/installation.ps1
+++ bin/installation.ps1
@@ -1,1 +1,41 @@
 Get-NetAdapter Ethernet | Disable-NetAdapter -Confirm:$false
+
+# INCLUDED IN REGISTRY_DISABLEDDRIVERS.POL
+<# 
+# HDAUDIO\FUNC_01&VEN_10DE&DEV_0084&SUBSYS_10DE1C03&REV_1001
+# HDAUDIO\FUNC_01&VEN_10DE&DEV_0084&SUBSYS_10DE1C03
+(Get-PnpDevice | ? FriendlyName -eq "NVIDIA High Definition Audio").HardwareID
+
+
+# MMDEVAPI\AudioEndpoints
+(Get-PnpDevice | ? FriendlyName -eq "XG2401 SERIES (NVIDIA High Definition Audio)").HardwareID
+
+
+# PCI\VEN_13F6&DEV_8788&SUBSYS_85211043&REV_00
+# PCI\VEN_13F6&DEV_8788&SUBSYS_85211043
+# PCI\VEN_13F6&DEV_8788&CC_040100
+# PCI\VEN_13F6&DEV_8788&CC_0401
+# MMDEVAPI\AudioEndpoints
+(Get-PnpDevice | ? FriendlyName -eq "Asus Xonar DGX Audio Device").HardwareID
+
+# PCI\VEN_10EC&DEV_8168&SUBSYS_86771043&REV_15
+# PCI\VEN_10EC&DEV_8168&SUBSYS_86771043
+# PCI\VEN_10EC&DEV_8168&CC_020000
+# PCI\VEN_10EC&DEV_8168&CC_0200
+(Get-PnpDevice | ? FriendlyName -eq "Realtek PCIe GbE Family Controller").HardwareID
+
+# Motherboard Audio (not dgx)
+# HDAUDIO\FUNC_01&VEN_10EC&DEV_0887&SUBSYS_104386C7&REV_1003
+# HDAUDIO\FUNC_01&VEN_10EC&DEV_0887&SUBSYS_104386C7
+(Get-PnpDevice | ? FriendlyName -like "High Definition Audio-Gerät").HardwareID
+
+# PCI\VEN_10DE&DEV_1C03&SUBSYS_1C0310DE&REV_A1
+# PCI\VEN_10DE&DEV_1C03&SUBSYS_1C0310DE
+# PCI\VEN_10DE&DEV_1C03&CC_030000
+# PCI\VEN_10DE&DEV_1C03&CC_0300
+(Get-PnpDevice | ? FriendlyName -eq "NVIDIA GeForce GTX 1060 6GB" | select HardwareId).HardwareId)
+
+gpupdate /force
+ #>
+ 
+ .\Import_ConfigFiles.ps1 -UseBasePOL -UsePolicy	
