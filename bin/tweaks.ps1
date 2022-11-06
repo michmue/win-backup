ef7627a75a4e4d6cb80298b95ef1b7fbded9e6d4
michael mueller <dev@michmue.de>
2022-11-06 12:02:14 +0100

    migrating.. tweaks *.reg to tweaks.ps1

---
+++ bin/tweaks.ps1
@@ -0,0 +1,438 @@
+<# TODO's: TWEAKS
+
+    ICONS
+        FileTypesMan
+        fileext (& filetype?)
+        .md
+        .xml
+        .json
+
+    REGISTRY
+        windows maximize?
+        search chances for netplwiz/autologin 
+            HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon 
+    
+    HIDE USERFOLDER
+        hide Shell Folders in Explorer & UserFolder?!?! https://www.winhelponline.com/blog/show-hide-shell-folder-namespace-windows-10/
+        
+    PASSWORDLESS LOGIN: 
+        HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\PasswordLess\Device
+        DevicePasswordLessBuildVersion DWORD 0 == Windows Hello Disabled
+        netplwiz.exe "User must enter a user name"
+#>
+
+enum Scope {
+    MACHINE
+    USER
+}
+
+class Tweak {
+    [string]$Name
+    [Scope]$Scope
+    [string]$RegContent
+}
+
+$tweaks = @(
+    [Tweak]@{ 
+        Name = "Disable_NewMenu_AccessDB"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+
+        [HKEY_CLASSES_ROOT\.mdb\ShellNew]
+        "Command"=-
+        "~Command"="msaccess.exe /NEWDB 1"
+'@}
+    
+    [Tweak]@{
+        Name = "Disable_NewMenu_AccessDB"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+
+        [HKEY_CLASSES_ROOT\.accdb\Access.Application.16\ShellNew]
+        "FileName"=-
+        "~FileName"="C:\\Program Files\\Microsoft Office\\root\\Office16\\1031\\ACCESS12.ACC"
+'@}
+    
+    [Tweak]@{
+        Name = "Disable_NewMenu_Visio"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+
+        [HKEY_CLASSES_ROOT\.vsdx\Visio.Drawing.15\ShellNew]
+        "FileName"=-
+        "~FileName"="C:\\Program Files\\Microsoft Office\\Root\\VFS\\Windows\\ShellNew\\visio.vsdx"
+'@}
+    
+    [Tweak]@{
+        Name = "Disable_NewMenu_RichTextFormat"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+
+        [HKEY_CLASSES_ROOT\.rtf\ShellNew]
+        "Data"=-
+        "ItemName"=-
+        "~Data"="{\\rtf1}"
+        "~ItemName"=hex(2):40,00,25,00,50,00,72,00,6f,00,67,00,72,00,61,00,6d,00,46,00,\
+          69,00,6c,00,65,00,73,00,25,00,5c,00,57,00,69,00,6e,00,64,00,6f,00,77,00,73,\
+          00,20,00,4e,00,54,00,5c,00,41,00,63,00,63,00,65,00,73,00,73,00,6f,00,72,00,\
+          69,00,65,00,73,00,5c,00,57,00,4f,00,52,00,44,00,50,00,41,00,44,00,2e,00,45,\
+          00,58,00,45,00,2c,00,2d,00,32,00,31,00,33,00,00,00
+'@}
+    
+    [Tweak]@{
+        Name = "Disable_NewMenu_Publisher"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+
+        [HKEY_CLASSES_ROOT\.pub\Publisher.Document.16\ShellNew]
+        "FileName"=-
+        "~FileName"="C:\\Program Files\\Microsoft Office\\Root\\VFS\\Windows\\ShellNew\\mspub.pub"
+'@}
+    
+    [Tweak]@{
+        Name = "Disable_NewMenu_PowerPoint"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+
+        [HKEY_CLASSES_ROOT\.pptx\PowerPoint.Show.12\ShellNew]
+        "FileName"=-
+        "~FileName"="C:\\Program Files\\Microsoft Office\\Root\\VFS\\Windows\\ShellNew\\powerpoint.pptx"
+'@}
+    
+    [Tweak]@{
+        Name = "Disable_NewMenu_MSProject"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+
+        [HKEY_CLASSES_ROOT\.mpp\MSProject.Project.9\ShellNew]
+        "FileName"=-
+        "~FileName"="C:\\Program Files\\Microsoft Office\\Root\\VFS\\Windows\\ShellNew\\msproj11.mpp"
+'@}
+    
+    [Tweak]@{
+        Name = "Disable_NewMenu_ZipFolder"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+
+        [HKEY_CLASSES_ROOT\.zip\CompressedFolder\ShellNew]
+        "Data"=-
+        "ItemName"=-
+        "~Data"=hex:50,4b,05,06,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00,00
+        "~ItemName"=hex(2):40,00,25,00,53,00,79,00,73,00,74,00,65,00,6d,00,52,00,6f,00,\
+          6f,00,74,00,25,00,5c,00,73,00,79,00,73,00,74,00,65,00,6d,00,33,00,32,00,5c,\
+          00,7a,00,69,00,70,00,66,00,6c,00,64,00,72,00,2e,00,64,00,6c,00,6c,00,2c,00,\
+          2d,00,31,00,30,00,31,00,39,00,34,00,00,00
+'@}
+    
+    [Tweak]@{
+        Name = "Disable_Folders_InExplorer"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+
+        ;; Created by: Shawn Brink
+        ;; Created on: April 13th 2018
+        ;; Tutorial: https://www.tenforums.com/tutorials/108032-hide-show-user-profile-personal-folders-windows-10-file-explorer.html
+
+
+        ;; DOWNLOADS
+        ;; [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{7d83ee9b-2244-4e70-b1f5-5393042af1e4}\PropertyBag]
+        ;; "ThisPCPolicy"="Hide"
+
+        ;; [HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{7d83ee9b-2244-4e70-b1f5-5393042af1e4}\PropertyBag]
+        ;; "ThisPCPolicy"="Hide"
+
+        ;; DESKTOP
+        ;; [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{B4BFCC3A-DB2C-424C-B029-7FE99A87C641}\PropertyBag]
+        ;; "ThisPCPolicy"="Hide"
+
+        ;; [HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{B4BFCC3A-DB2C-424C-B029-7FE99A87C641}\PropertyBag]
+        ;; "ThisPCPolicy"="Hide"
+
+        ;; [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{754AC886-DF64-4CBA-86B5-F7FBF4FBCEF5}\PropertyBag]
+        ;; "ThisPCPolicy"="Hide"
+
+        ;; [HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{754AC886-DF64-4CBA-86B5-F7FBF4FBCEF5}\PropertyBag]
+        ;; "ThisPCPolicy"="Hide"
+
+        ;; 3D-OBJECTS
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{31C0DD25-9439-4F12-BF41-7FF4EDA38722}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{31C0DD25-9439-4F12-BF41-7FF4EDA38722}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+
+        ;; SAVEDGAMES
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{4C5C32FF-BB9D-43b0-B5B4-2D72E54EAAA4}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{4C5C32FF-BB9D-43b0-B5B4-2D72E54EAAA4}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+
+        ;; FAVORITES
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{1777F761-68AD-4D8A-87BD-30B759FA33DD}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{1777F761-68AD-4D8A-87BD-30B759FA33DD}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+
+        ;; LINKS
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{bfb9d5e0-c6a9-404c-b2b2-ae6db6af4968}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{bfb9d5e0-c6a9-404c-b2b2-ae6db6af4968}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+
+        ;; SEARCHES
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{7d1d3a04-debb-4115-95cf-2f29da2920da}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{7d1d3a04-debb-4115-95cf-2f29da2920da}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+
+        ;; CONTACTS
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{56784854-C6CB-462B-8169-88E350ACB882}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{56784854-C6CB-462B-8169-88E350ACB882}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+
+        ;; VIDEOS
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{35286a68-3c57-41a1-bbb1-0eae73d76c95}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{35286a68-3c57-41a1-bbb1-0eae73d76c95}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+
+        ;; DOCUMENTS
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{f42ee2d3-909f-4907-8871-4c22fc0bf756}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{f42ee2d3-909f-4907-8871-4c22fc0bf756}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+
+        ;; FAVORITES
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{1777F761-68AD-4D8A-87BD-30B759FA33DD}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{1777F761-68AD-4D8A-87BD-30B759FA33DD}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+
+        ;; MUSIC
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{a0c69a99-21c8-4671-8703-7934162fcf1d}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{a0c69a99-21c8-4671-8703-7934162fcf1d}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+
+        ;; PICTURES
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{0ddd015d-b06c-45d5-8c4c-f59713854639}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{0ddd015d-b06c-45d5-8c4c-f59713854639}\PropertyBag]
+        "ThisPCPolicy"="Hide"
+'@}
+    [Tweak]@{
+        Name = "Set_StartupDelayForPrograms_toZero"; 
+        Scope = [Scope]::USER
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+
+        [HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Serialize]
+        "StartupDelayInMSec"=dword:00000000
+'@}
+
+
+    [Tweak]@{
+        Name = "Block_ContextMenu_PreviouseVersion"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked]
+        "{596AB062-B4D2-4215-9F74-E9109B0A8153}"="PreviouseVersion"
+'@}
+
+    [Tweak]@{
+        Name = "Block_ContextMenu_TroubleshootCompatibility"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+        
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked]
+        "{1d27f844-3a1f-4410-85ac-14651078412d}"="TroubleshootCompatibility"
+'@}
+
+    [Tweak]@{
+        Name = "Block_ContextMenu_Share"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+        
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked]
+        "{e2bf9676-5f8f-435c-97eb-11607a5bedf7}"="Share"
+'@}
+
+    [Tweak]@{
+        Name = "Block_ContextMenu_ScanWithDefender"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+        
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked]
+        "{09A47860-11B0-4DA5-AFA5-26D86198A780}"="ScanWithDefender"
+'@}
+  
+    [Tweak]@{
+        Name = "Delete_ContextMenu_PinToTaskbar"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+        
+        [-HKEY_CLASSES_ROOT\*\shellex\ContextMenuHandlers\{90AA3A4E-1CBA-4233-B8BB-535773D48449}]
+'@}  
+    
+    [Tweak]@{
+        Name = "Delete_ContextMenu_IncludeInLibrary"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+
+        [-HKEY_CLASSES_ROOT\Folder\ShellEx\ContextMenuHandlers\Library Location]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Folder\ShellEx\ContextMenuHandlers\Library Location]
+'@}
+    
+    [Tweak]@{
+        Name = "Delete_ContextMenu_PinToQuickAccess"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+        
+        [-HKEY_CLASSES_ROOT\Folder\shell\pintohome]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\Folder\shell\pintohome]
+'@}
+    
+    [Tweak]@{
+        Name = "Delete_ContextMenu_SentTo"
+        Scope = [Scope]::MACHINE
+        RegContent =@'
+        Windows Registry Editor Version 5.00
+
+        [-HKEY_CLASSES_ROOT\AllFilesystemObjects\shellex\ContextMenuHandlers\SendTo]
+        [-HKEY_CLASSES_ROOT\UserLibraryFolder\shellex\ContextMenuHandlers\SendTo]
+'@}
+    
+    [Tweak]@{
+        Name = "Delete_ContextMenu_PinToStartScreen"; 
+        Scope = [Scope]::MACHINE
+        RegContent = @'
+        Windows Registry Editor Version 5.00
+        
+        [-HKEY_CLASSES_ROOT\Folder\shellex\ContextMenuHandlers\PintoStartScreen]
+        [-HKEY_CLASSES_ROOT\exefile\shellex\ContextMenuHandlers\PintoStartScreen]
+        [-HKEY_CLASSES_ROOT\Microsoft.Website\ShellEx\ContextMenuHandlers\PintoStartScreen]
+        [-HKEY_CLASSES_ROOT\mscfile\shellex\ContextMenuHandlers\PintoStartScreen
+'@}
+    
+    [Tweak]@{
+        # HINT: NonEnum can only hide TopLevel Items in Explorer SideBar
+        Name = "Hide_Explorer_TopLevelDrives"
+        Scope = [Scope]::USER
+        RegContent =@'
+        Windows Registry Editor Version 5.00
+
+        [HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\NonEnum]
+        "{F5FB2C77-0E2F-4A16-A381-3E560C68BC83}"=dword:00000001
+'@}
+    
+    [Tweak]@{
+        Name = "Delete_ContextMenu_EditWithPaint3D"
+        Scope = [Scope]::MACHINE
+        RegContent =@'
+        Windows Registry Editor Version 5.00
+
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.3mf\Shell\3D Edit]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.bmp\Shell\3D Edit]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.fbx\Shell\3D Edit]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.gif\Shell\3D Edit]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.glb\Shell\3D Edit]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.jfif\Shell\3D Edit]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.jpe\Shell\3D Edit]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.jpeg\Shell\3D Edit]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.jpg\Shell\3D Edit]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.obj\Shell\3D Edit]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.ply\Shell\3D Edit]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.png\Shell\3D Edit]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.stl\Shell\3D Edit]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.tif\Shell\3D Edit]
+        [-HKEY_LOCAL_MACHINE\SOFTWARE\Classes\SystemFileAssociations\.tiff\Shell\3D Edit]
+'@}    
+    
+    [Tweak]@{
+        Name = "Delete_ContextMenu_EditWithPaint3D"
+        Scope = [Scope]::MACHINE
+        RegContent =@'
+        Windows Registry Editor Version 5.00
+
+        [HKEY_CLASSES_ROOT\Allfilesystemobjects\shell\windows.copyaspath]
+        @="&Pfad kopieren"
+        "Icon"="imageres.dll,-5302"
+        "InvokeCommandOnSelection"=dword:00000001
+        "VerbHandler"="{f3d06e7c-1e45-4a26-847e-f9fcdee59be0}"
+        "VerbName"="copyaspath"
+'@}
+    [Tweak]@{
+        Name = "Block_ContextMenu_GiveAccessTo"
+        Scope = [Scope]::MACHINE
+        RegContent =@'
+        Windows Registry Editor Version 5.00
+        
+        [HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Shell Extensions\Blocked]
+        "{f81e9010-6ea4-11ce-a7ff-00aa003ca9f6}"="GiveAccessTo"
+'@}
+    [Tweak]@{
+        Name = "Replace_Notepad_With_Notepad++"
+        Scope = [Scope]::MACHINE
+        RegContent =@'
+        Windows Registry Editor Version 5.00
+
+        [HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\notepad.exe]
+        "Debugger"="\"C:\\Program Files\\Notepad++\\notepad++.exe\" -notepadStyleCmdline -z"
+'@}
+)
+
+
+
+
+foreach ($tweak in $tweaks)
+{
+    $tweak.RegContent = $tweak.RegContent.Replace("        ", "");
+}
+
+
+foreach ($tweak in $tweaks)
+{
+    echo $tweak.RegContent | Out-File -FilePath $PSScriptRoot\tweak.reg
+    reg.exe import "$($PSScriptRoot)\tweak.reg" 2>$null
+}
+
+Remove-Item $PSScriptRoot\tweak.reg
+
